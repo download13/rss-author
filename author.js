e7e0ac83e9fc9e4adf464e0ade2a4963f3f8e8f8
Erin Dachtler <download333@gmail.com>
2014-09-21 15:07:04 -0700

    Initial commit

---
+++ author.js
@@ -0,0 +1,60 @@
+var parseOneAddress = require('email-addresses').parseOneAddress;
+
+var separators = /[\<\>\(\)]+/g;
+var separated = /<(.+)>|\((.+)\)/;
+
+
+function parseAuthor(str) {
+	var email = null, name = null;
+	str = str.trim();
+
+	// Stuff comes in looking like this sometimes: email@domain.tld (Person Surname)
+	// We should account for variations though (<> instead of (), switched order)
+	var segments = separated.exec(str);
+
+	// If there's something inside markers
+	if(segments) {
+		var inside = segments[1] || segments[2];
+		var outside = str
+			.replace(inside, '') // Remove the marker contents
+			.replace(separators, '') // Remove the markers
+			.trim();
+
+		email = parseOneAddress(inside); // See if inside is email
+
+		if(email) { // Turns out it was
+			email = email.address; // get address, and
+			name = outside; // assume outside is name
+		} else {
+			name = inside; // Assume inside is name
+			email = parseOneAddress(outside); // and outside is address
+
+			if(email) {
+				email = email.address;
+			} else { // Neither are email
+				email = null; // Maybe this one doesn't contain an email
+				name = str; // Just a name
+			}
+		}
+	} else { // Otherwise, just parse it as email
+		email = parseOneAddress(str);
+
+		if(email) {
+			email = email.address;
+		} else { // Not an email, maybe just a name
+			email = null;
+			name = str;
+		}
+	}
+
+	if(!name) name = null;
+	if(!email) email = null;
+
+	return {
+		name: name,
+		email: email
+	};
+}
+
+
+module.exports = parseAuthor;
---
+++ test.js
@@ -0,0 +1,110 @@
+require('blanket')({
+	pattern: function(filename) {
+		console.log(filename);
+		return filename.indexOf('author.js') !== -1;
+	}
+});
+
+var assert = require('assert');
+var parseAuthor = require('./author');
+
+
+describe('rss-author', function() {
+	it('should find an address and name: email (name)', function() {
+		var r = parseAuthor('test@example.com (Testy McTesterson)');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: email <name>', function() {
+		var r = parseAuthor('test@example.com <Testy McTesterson>');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: (name) email', function() {
+		var r = parseAuthor('(Testy McTesterson) test@example.com');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: <name> email', function() {
+		var r = parseAuthor('<Testy McTesterson> test@example.com');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: name <email>', function() {
+		var r = parseAuthor('Testy McTesterson <test@example.com>');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: <email> name', function() {
+		var r = parseAuthor('<test@example.com> Testy McTesterson');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: name (email)', function() {
+		var r = parseAuthor('Testy McTesterson (test@example.com)');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find an address and name: (email) name', function() {
+		var r = parseAuthor('(test@example.com) Testy McTesterson');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, 'test@example.com');
+	});
+
+	it('should find a raw name', function() {
+		var r = parseAuthor('Testy McTesterson');
+
+		assert.equal(r.name, 'Testy McTesterson');
+		assert.equal(r.email, null);
+	});
+
+	it('should find a name in gibberish', function() {
+		var r = parseAuthor('ajdfdausifyiua89fd89fa89dfhjadfjk');
+
+		assert.equal(r.name, 'ajdfdausifyiua89fd89fa89dfhjadfjk');
+		assert.equal(r.email, null);
+	});
+
+	it('should find a name with () in it', function() {
+		var r = parseAuthor('Testy (mr wut) McTesterson');
+
+		assert.equal(r.name, 'Testy (mr wut) McTesterson');
+		assert.equal(r.email, null);
+	});
+
+	it('should find a raw address', function() {
+		var r = parseAuthor('an@address.net');
+
+		assert.equal(r.email, 'an@address.net');
+		assert.equal(r.name, null);
+	});
+
+	it('should find an address with ()', function() {
+		var r = parseAuthor('(an@address.net)');
+
+		assert.equal(r.email, 'an@address.net');
+		assert.equal(r.name, null);
+	});
+
+	it('should find an address with <>', function() {
+		var r = parseAuthor('<an@address.net>');
+
+		assert.equal(r.email, 'an@address.net');
+		assert.equal(r.name, null);
+	});
+});
